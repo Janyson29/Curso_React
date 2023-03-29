834578dd615b0d0a4a491a709757ade37bfbe6b6
Janyson Marques <janyson29@hotmail.com>
2023-03-29 17:24:12 -0300

    introdução do react

---
+++ desafio_1/desafio1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="p">Desafio 01</p>
+      <h3>desafio 01</h3>
+    </div>
+  );
+}
+
+export default App;
