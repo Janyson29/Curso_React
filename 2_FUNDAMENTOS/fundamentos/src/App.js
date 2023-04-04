d8c026ff561b496bb2b73bbc2f48b8df9ceec50b
Janyson Marques <janyson29@hotmail.com>
2023-04-04 19:54:48 -0300

    Criando componentes

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+// componentes
+import FirstComponet from "./componets/FirstComponets"
+
+// styles / css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos</h1>
+      <FirstComponet></FirstComponet>
+    </div>
+  );
+}
+
+export default App;
