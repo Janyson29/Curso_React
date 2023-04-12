3796f38a89f00ded23f2f2d61d7332bbd4944cec
Janyson Marques <janyson29@hotmail.com>
2023-04-11 22:17:36 -0300

    Conhecendo o JSX

--- 2_FUNDAMENTOS/fundamentos/src/componets/FirstComponets.js
+++ 2_FUNDAMENTOS/fundamentos/src/componets/FirstComponets.js
@@ -2,6 +2,8 @@ const FirstComponet = () => {
     return (
         <div>
             <h1>Meu primeiro componente</h1>
+            <p className="test">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <div className="test2">kjdkj</div>
         </div>
     );
 };
