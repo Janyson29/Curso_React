c9ff7298b3db852168020302e53f29ea62819bd6
Janyson Marques <janyson29@hotmail.com>
2023-04-13 20:15:53 -0300

    Comentários nos componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -4,9 +4,17 @@ import FirstComponet from "./componets/FirstComponets"
 // styles / css
 import './App.css';
 
+// comentário
 function App() {
+
+  // comentário 1
+
+  /* 
+  comentário 2
+  */
   return (
     <div className="App">
+      {/*comentário*/}
       <h1>Fundamentos</h1>
       <FirstComponet></FirstComponet>
     </div>
