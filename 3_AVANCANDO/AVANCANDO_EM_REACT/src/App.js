aed01d0d2ab1f1b49ab8191bc30fd1284a2b05ec
Janyson Marques <janyson29@hotmail.com>
2023-04-14 23:50:30 -0300

    img

--- 3_AVANCANDO/AVANCANDO_EM_REACT/src/App.js
+++ 3_AVANCANDO/AVANCANDO_EM_REACT/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import city from "./assets/city.jpg"
 
 function App() {
   return (
     <div className="App">
       <h1>Seção 3</h1>
+      {/* img em public */}
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
+      </div>
+      <img src={city} alt="dfgdf" />
     </div>
   );
 }
