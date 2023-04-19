24dca4b17b917def6f13e929534e68d725c2aee7
Janyson Marques <janyson29@hotmail.com>
2023-04-19 18:18:21 -0300

    A propriedade key

--- 3_AVANCANDO/AVANCANDO_EM_REACT/src/componets/ListRender.js
+++ 3_AVANCANDO/AVANCANDO_EM_REACT/src/componets/ListRender.js
@@ -3,11 +3,22 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Mateus", 'pEDRO', 'JOÃO', 'Maria'])
 
+    const [users] = useState([
+        { id: 1, name: 'Matheus', age: 31},
+        { id: 341634, name: 'Pedro', age: 25},
+        { id: 546464, name: 'Maria', age: 22}
+    ])
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}> {user.name} - {user.age}</li>
             ))}
         </ul>
     </div>
