b558fd409f821a4b1d004643d833eb9b26ad590c
Janyson Marques <janyson29@hotmail.com>
2023-04-28 20:18:48 -0300

    Previous state

--- 3_AVANCANDO/AVANCANDO_EM_REACT/src/componets/ListRender.js
+++ 3_AVANCANDO/AVANCANDO_EM_REACT/src/componets/ListRender.js
@@ -3,11 +3,22 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Mateus", 'pEDRO', 'JOÃO', 'Maria'])
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: 'Matheus', age: 31},
-        { id: 341634, name: 'Pedro', age: 25},
-        { id: 546464, name: 'Maria', age: 22}
-    ])
+        { id: 2, name: 'Pedro', age: 25},
+        { id: 3, name: 'Maria', age: 22}
+    ]);
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4)
+
+        setUsers((prevUsers) => {
+            console.log(prevUsers)
+
+            return prevUsers.filter((user) => randomNumber !== user.id)
+
+        })
+    }
 
   return (
     <div>
@@ -21,6 +32,7 @@ const ListRender = () => {
                 <li key={user.id}> {user.name} - {user.age}</li>
             ))}
         </ul>
+        <button onClick={deleteRandom}>Delete Random User</button>
     </div>
   )
 }
