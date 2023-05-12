c497b6712693cc30097eca0cebd2d4d5602e687d
Janyson Marques <janyson29@hotmail.com>
2023-05-11 22:05:39 -0300

    Renderização de lista em componente

--- 3_AVANCANDO/AVANCANDO_EM_REACT/src/App.js
+++ 3_AVANCANDO/AVANCANDO_EM_REACT/src/App.js
@@ -11,6 +11,12 @@ function App() {
   const sobrenome = "Marques";
   const [userName] = useState("Maria")
 
+  const cars = [
+    { id: 1, brand: "ferrari", color: "amarelo", newCar: true, km: 0 },
+    { id: 2, brand: "kia", color: "verde", newCar: false, km: 25454 },
+    { id: 3, brand: "renault", color: "azul", newCar: false, km: 55454 }
+  ];
+
   return (
     <div className="App">
       <h1>Seção 3</h1>
@@ -25,14 +31,21 @@ function App() {
       <ManageData></ManageData>
       <ListRender></ListRender>
       <ConditionalRender></ConditionalRender>
+
       {/* props */}
       <ShowUserName name="Mateus" sobrenome={sobrenome} userName={userName} />
+
       {/* destructuring */}
       <CarDetails brand="w" km={10000} color="Azul"  newCar={false}/>
+
       {/* reaproveitando  */}
       <CarDetails brand='ford' km={0} color='rosa' newCar={true} />
       <CarDetails brand='fiat' km={200000} color='prata'  newCar={false}/>
 
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} km={car.km} color={car.color}  newCar={car.newCar}/>
+      ))}
     </div>
   );
 }
