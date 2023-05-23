6a801feda8e18cdb2904a95a56e27fa1209587c2
Janyson Marques <janyson29@hotmail.com>
2023-05-23 17:55:38 -0300

    CSS inline dinâmico

--- 4_css-react/css/src/App.js
+++ 4_css-react/css/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from 'react'
 
 function App() {
+  const n = 15
+  const [name] = useState('matheus')
   return (
     <div className="App">
       {/* CSS Global */}
@@ -11,6 +14,11 @@ function App() {
       <p>Esse parágrafo é do app.js</p>
       {/* Inline css */}
       <p style={{color: 'blue', padding: '25px', borderTop: '2px solid red'}}>Esse elemento foi estilizado de forma inline</p>
+      {/* CSS inline dinâmico */}
+      <h2 style={n < 10 ? {color: 'purple'} : {color: 'pink'}}>CSS Dinâmico</h2>
+      <h2 style={n > 10 ? {color: 'purple'} : {color: 'pink'}}>CSS Dinâmico</h2>
+      <h2 style={name === 'matheus' ? {color: 'green', backgroundColor: 'red'} : null }>teste nome</h2>
+
     </div>
   );
 }
