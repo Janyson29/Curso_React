4455e8081e9e100fc38dc53c4522b06e8d8fe17a
Janyson Marques <janyson29@hotmail.com>
2023-05-23 18:44:10 -0300

    Classes dinâmicas

--- 4_css-react/css/src/App.js
+++ 4_css-react/css/src/App.js
@@ -5,6 +5,8 @@ import { useState } from 'react'
 function App() {
   const n = 15
   const [name] = useState('matheus')
+
+  const redTitle = false
   return (
     <div className="App">
       {/* CSS Global */}
@@ -18,7 +20,8 @@ function App() {
       <h2 style={n < 10 ? {color: 'purple'} : {color: 'pink'}}>CSS Dinâmico</h2>
       <h2 style={n > 10 ? {color: 'purple'} : {color: 'pink'}}>CSS Dinâmico</h2>
       <h2 style={name === 'matheus' ? {color: 'green', backgroundColor: 'red'} : null }>teste nome</h2>
-
+      {/* classe dinâmica */}
+      <h2 className={redTitle ? 'red-title' : 'title' }>Esse título vai ter classe dinâmica</h2>
     </div>
   );
 }
