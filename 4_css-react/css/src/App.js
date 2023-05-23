f4e3fa64571f7e26a50ed3b5216f2b5d35134552
Janyson Marques <janyson29@hotmail.com>
2023-05-23 17:28:34 -0300

    CSS de componentes

--- 4_css-react/css/src/App.js
+++ 4_css-react/css/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
       <h1>Css do react</h1>
+      {/* CSS Component */}
+      <MyComponent />
+      <p>Esse parágrafo é do app.js</p>
     </div>
   );
 }
---
+++ 4_css-react/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css'
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>CSS de component</h1>
+      <p>Esse é o parágrafo do component</p>
+      <p className="esse-paragrafo-e-do-component">Esse tb é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
