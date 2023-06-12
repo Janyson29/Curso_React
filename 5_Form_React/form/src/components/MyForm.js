ac27f87fe6654f32a710f0ab6014658824edf1eb
Janyson Marques <janyson29@hotmail.com>
2023-06-12 11:41:01 -0300

    Textarea no React

--- 5_Form_React/form/src/components/MyForm.js
+++ 5_Form_React/form/src/components/MyForm.js
@@ -7,6 +7,8 @@ const MyForm = ({user}) => {
   const [name, setName] = useState(user ? user.name : '');
   const [email, setEmail] = useState(user ? user.email : '');
 
+  const [bio, setBio] = useState('')
+
   const handleName = (e) => {
     setName(e.target.value);
   };
@@ -17,7 +19,7 @@ const MyForm = ({user}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('enviando o formulário');
-    console.log(name, email);
+    console.log(name, email, bio);
 
     // validação
     // envio
@@ -25,6 +27,7 @@ const MyForm = ({user}) => {
     // 7- limpar form
     setName('');
     setEmail('');
+    setBio('')
   }
 
   return (
@@ -51,6 +54,17 @@ const MyForm = ({user}) => {
           onChange={(e) => setEmail(e.target.value)} 
           value={email}
           />
+        </label>
+        {/* 8 - Textarea */}
+        <label>
+          <span>Bio:</span>
+          <textarea 
+          name="bio" 
+          placeholder='Descrição do usuário' 
+          onChange={(e) => setBio(e.target.value)} 
+          value={bio}
+          >
+          </textarea>
         </label>
         <input type="submit" value="Enviar" />
       </form>
