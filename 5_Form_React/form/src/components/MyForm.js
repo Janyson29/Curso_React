a3842a99e9f70d384cfb5cb3c8cb9ff77bc54432
Janyson Marques <janyson29@hotmail.com>
2023-06-12 11:29:14 -0300

    Controlled inputs

--- 5_Form_React/form/src/components/MyForm.js
+++ 5_Form_React/form/src/components/MyForm.js
@@ -1,10 +1,11 @@
 import './MyForm.css'
 import {useState} from 'react'
 
-const MyForm = () => {
+const MyForm = ({user}) => {
+  // 6 - controlled inputs
   // 3 - gerenciamento de dados
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
+  const [name, setName] = useState(user ? user.name : '');
+  const [email, setEmail] = useState(user ? user.email : '');
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -25,12 +26,24 @@ const MyForm = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nome: </label>
-          <input type="text" name='name' placeholder="digite o seu nome" onChange={handleName}/>
+          <input 
+          type="text" 
+          name='name' 
+          placeholder="digite o seu nome" 
+          onChange={handleName} 
+          value={name}
+          />
         </div>
         {/* 2- Label envolvendo um input */}
         <label>
           <span>E-mail</span>
-          <input type="email" name="email" placeholder='digite o seu e-mail' onChange={(e) => setEmail(e.target.value)} />
+          <input 
+          type="email" 
+          name="email" 
+          placeholder='digite o seu e-mail' 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+          />
         </label>
         <input type="submit" value="Enviar" />
       </form>
