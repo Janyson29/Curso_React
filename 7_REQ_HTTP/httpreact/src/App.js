a4792612cbeebecc06c26d495213be9280325294
Janyson Marques <janyson29@hotmail.com>
2023-06-22 21:24:43 -0300

    Resgatando dados da API

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+import {useState, useEffect} from 'react';
+
+// const url = 'http://localhost:3001/products';
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const res = await fetch("http://localhost:3001/products");
+
+      const data = await res.json();
+
+      setProducts(data);
+
+      return (data)
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+
+      <ul>
+        {products.map((product) => (  
+          <li key={product.id}>
+            {product.name}
+            -
+            {product.price}
+          </li>
+
+        ))}
+      </ul>
     </div>
   );
 }
